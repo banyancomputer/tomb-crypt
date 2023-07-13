8ba5bd5da76cdc173f68c95a94a26a0653a8903d
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-07-13 12:27:47 -0400

    feat: further developments on aes-kw, need to test compatibility

--- src/crypto.rs
+++ src/crypto.rs
@@ -7,7 +7,7 @@ use openssl::pkey::{PKey, Private, Public};
 use openssl::rand;
 use openssl::sha::sha1;
 
-const SALT_SIZE: usize = 8;
+pub(crate) const SALT_SIZE: usize = 8;
 
 pub(crate) type CryptoResult<T> = Result<T, String>;
 
@@ -85,9 +85,15 @@ pub(crate) fn generate_key() -> CryptoResult<PKey<Private>> {
     Ok(private_key)
 }
 
-pub(crate) fn hkdf(derived_bits: &[u8]) -> CryptoResult<(Vec<u8>, Vec<u8>)> {
-    let mut salt: [u8; SALT_SIZE] = [0; SALT_SIZE];
-    rand::rand_bytes(&mut salt).map_err(|err| format!("unable to generate random IV: {err:?}"))?;
+pub(crate) fn hkdf(derived_bits: &[u8], raw_salt: Option<[u8; SALT_SIZE]>) -> CryptoResult<(Vec<u8>, Vec<u8>)> {
+    let salt = match raw_salt {
+        Some(s) => s,
+        None => {
+            let mut salt = [0u8; SALT_SIZE];
+            rand::rand_bytes(&mut salt).map_err(|err| format!("unable to generate random IV: {err:?}"))?;
+            salt
+        }
+    };
 
     // Note: this must be < 1024 chars and I should generate it base on other known data
     let info: &str = "fixed-data-todo";
@@ -118,4 +124,28 @@ pub(crate) fn public_key(private_key: &PKey<Private>) -> CryptoResult<PKey<Publi
 
     PKey::from_ec_key(pub_ec)
         .map_err(|err| format!("unable to create public PKey from public EcKey: {err:?}"))
+}
+
+use openssl::aes::{AesKey, unwrap_key, wrap_key};
+
+pub(crate) fn our_wrap_key(secret: &[u8], unprotected_key: &[u8]) -> CryptoResult<Vec<u8>> {
+    let wrapping_key = AesKey::new_encrypt(secret)
+        .map_err(|err| format!("unable to use secret for key wrapping: {err:?}"))?;
+
+    let mut cipher_text = [0u8; 40];
+    wrap_key(&wrapping_key, None, &mut cipher_text, &unprotected_key)
+        .map_err(|err| format!("unable to wrap key: {err:?}"))?;
+
+    Ok(cipher_text.to_vec())
+}
+
+pub(crate) fn our_unwrap_key(secret: &[u8], protected_key: &[u8]) -> CryptoResult<Vec<u8>> {
+    let wrapping_key = AesKey::new_decrypt(secret)
+        .map_err(|err| format!("unable to use secret for key unwrapping: {err:?}"))?;
+
+    let mut plain_text = [0u8; 32];
+    unwrap_key(&wrapping_key, None, &mut plain_text, &protected_key)
+        .map_err(|err| format!("unable to wrap key: {err:?}"))?;
+
+    Ok(plain_text.to_vec())
 }
--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,37 @@ fn main() -> crypto::CryptoResult<()> {
         crypto::fingerprint(&ephemeral_public)?
     );
 
-    let ecdh_bytes = crypto::ecdh(&primary_private, &ephemeral_public)?;
+    // First round
+
+    let ecdh_bytes = crypto::ecdh(&ephemeral_private, &primary_public)?;
     println!("Raw ECDH Bytes: {}", B64.encode(&ecdh_bytes));
 
-    let (salt, hkdf_bytes) = crypto::hkdf(&ecdh_bytes)?;
+    let (salt, hkdf_bytes) = crypto::hkdf(&ecdh_bytes, None)?;
     println!(
         "HKDF Enhanced Bits: {}.{}",
-        B64.encode(salt),
-        B64.encode(hkdf_bytes)
+        B64.encode(salt.as_slice()),
+        B64.encode(hkdf_bytes.as_slice())
     );
+
+    // These are the bytes that make up the WNFS temporal key, this value is used as a placeholder
+    const plaintext_temporal_key: &[u8; 32] = b"demo-key-do-not-reuse-sample-key";
+
+    let wrapped_key = crypto::our_wrap_key(hkdf_bytes.as_slice(), plaintext_temporal_key.as_ref())?;
+    println!("Wrapped Key: {}", B64.encode(&wrapped_key));
+
+    // Now lets try and recover from the other side
+
+    let recovered_ecdh_bytes = crypto::ecdh(&primary_private, &ephemeral_public)?;
+    assert_eq!(ecdh_bytes, recovered_ecdh_bytes, "derived keys to be the same");
+
+    let mut salt_bytes = [0u8; crypto::SALT_SIZE];
+    salt_bytes.copy_from_slice(salt.as_slice());
+    let (_, recovered_hkdf_bytes) = crypto::hkdf(&recovered_ecdh_bytes, Some(salt_bytes))?;
+
+    let unwrapped_key = crypto::our_unwrap_key(recovered_hkdf_bytes.as_slice(), wrapped_key.as_slice())?;
+    assert_eq!(plaintext_temporal_key, unwrapped_key.as_slice(), "unencrypted temporal key should be the same");
+
+    println!("Unwrapped Key: {}", String::from_utf8_lossy(unwrapped_key.as_slice()));
 
     Ok(())
 }
